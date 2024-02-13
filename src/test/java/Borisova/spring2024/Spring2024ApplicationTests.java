8e5349c36d8cdacc0cfbc9a197fd919c3af050e0
Evgenia73 <evgeniaborisova02@gmail.com>
2024-02-13 21:56:25 +0500

    git init

---
+++ src/test/java/Borisova/spring2024/Spring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package Borisova.spring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
